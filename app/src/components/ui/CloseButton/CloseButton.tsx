2ac5d5b423c4084791a8d2327d9b8a59be4a0655
Macoix <giusemedina@gmail.com>
2023-03-12 14:19:25 -0500

    Feat: adding types to closeButton componnet

--- app/src/components/ui/CloseButton/CloseButton.tsx
+++ app/src/components/ui/CloseButton/CloseButton.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import { HiX } from 'react-icons/hi'
 import classNames from 'classnames'
 
-const CloseButton = React.forwardRef((props, ref) => {
+interface CloseButtonProps {
+  absolute?: boolean
+  className?: string
+  defaultStyle?: boolean
+  svgClass?: string
+  onClick?: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  res?: any
+}
 
-	const { absolute, className, defaultStyle, svgClass, ...rest } = props
+const CloseButton = React.forwardRef((props: CloseButtonProps, ref: React.ForwardedRef<HTMLSpanElement>) => {
+
+	const { absolute, className, defaultStyle, svgClass, onClick,...rest } = props
 	const closeButtonAbsoluteClass = 'absolute z-10'
 
 	const closeButtonClass = classNames(
@@ -14,12 +23,11 @@ const CloseButton = React.forwardRef((props, ref) => {
 		className
 	)
 
-  console.log(ref);
-
 	return (
 		<span
 			className={closeButtonClass}
 			role="button"
+      onClick={onClick}
 			{...rest}
 			ref={ref}
 		>
